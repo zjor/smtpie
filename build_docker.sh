065bc03eec47a88e085f27f461b370c358b267c9
Sergey Royz <sergey@ext.latch.com>
2020-07-17 18:09:42 +0200

    script update

--- build_docker.sh
+++ build_docker.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+IMAGE_NAME="smtpie:latest"
+DOCKER_ID_USER="zjor"
+
 set -x 
 
-docker build -t smtpie ./app
+docker build -t ${IMAGE_NAME} ./app
+
+docker tag ${IMAGE_NAME} ${DOCKER_ID_USER}/${IMAGE_NAME}

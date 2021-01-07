4d76090a47311fe4c0a22e3fca6accb19de594d2
Sergey Royz <zjor.se@gmail.com>
2021-01-07 20:10:23 +0100

    wip

--- app/config.py
+++ app/config.py
@@ -1,16 +1,36 @@
-# app/config.py
-
 from os import environ
-from dotenv import load_dotenv
 
-load_dotenv('.env')
+CONFIG_FILE = environ.get('CONFIG_FILE')
 
-SECRET_KEY = environ.get('SECRET_KEY')
-MAIL_SERVER = environ.get('MAIL_SERVER')
-MAIL_PORT = environ.get('MAIL_PORT')
-MAIL_USERNAME = environ.get('MAIL_USERNAME')
-MAIL_PASSWORD = environ.get('MAIL_PASSWORD')
-MAIL_DEFAULT_SENDER = environ.get('MAIL_DEFAULT_SENDER')
-SECURITY_EMAIL_SENDER = environ.get('SECURITY_EMAIL_SENDER')
-MAIL_USE_SSL = environ.get('MAIL_USE_SSL')
 
+class Config:
+    def __init__(self, name: str, app_id: str, secret: str, server: str, port: int, use_ssl: bool, username: str,
+                 password: str):
+        self.name = name
+        self.app_id = app_id
+        self.secret = secret
+        self.server = server
+        self.port = port
+        self.use_ssl = use_ssl
+        self.username = username
+        self.password = password
+
+    def __str__(self):
+        return self.__dict__.__str__()
+
+
+def load_from_yaml(filename):
+    import yaml
+    doc = yaml.load(open(filename, 'r'))
+    apps = {}
+    for e in doc['applications']:
+        config = Config(name=e['name'],
+                        app_id=e['appId'],
+                        secret=e['secret'],
+                        server=e['connection']['server'],
+                        port=e['connection']['port'],
+                        use_ssl=e['connection']['ssl'],
+                        username=e['credentials']['username'],
+                        password=e['credentials']['password'])
+        apps[config.app_id] = config
+    return apps

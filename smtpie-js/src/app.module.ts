bce38e28a143c9555857bfbb3bdaff80fd61fb75
Sergey Royz <zjor.se@gmail.com>
2021-02-22 20:19:42 +0100

    wip

--- smtpie-js/src/app.module.ts
+++ smtpie-js/src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ApiController } from './api/api.controller';
 import { TemplateService } from './template-service/template.service';
+import { ConfigService } from './config/config.service';
 
 @Module({
   imports: [],
   controllers: [AppController, ApiController],
-  providers: [AppService, TemplateService],
+  providers: [AppService, TemplateService, ConfigService],
 })
 export class AppModule {}
